74651a0ead67eb61059c42c7cbbf76bbdc10aad1
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-25 11:08:27 +0100

    Arbeiten mit Variablen

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -10,7 +10,55 @@ namespace HalloWelt
         static void Main(string[] args)
         {
             // Kommentar : Keine Befehle, sondern Beschreibung
-            Console.WriteLine("HalloWelt");
+            Console.WriteLine("Hallo Welt");
+
+            // Ganze Zahlen
+            int meineZahl = 12;
+            int a = 5, b = 10, c = 15;
+
+            // Zeichenketten
+            string test = "testString";
+            string t1 = "Hallo", t2 = "Welt", t3 = "!";
+
+            // Gleitkommazahl:
+            double pi = 3.14; // punkt als Komma
+
+            // Neue Werte zuweisen:
+
+            meineZahl = 42;
+            meineZahl = 60;
+            meineZahl = 0;
+
+            Console.WriteLine(meineZahl);
+
+            Console.WriteLine($"Der aktuelle Wert von meineZahl ist: {meineZahl}");
+            Console.WriteLine("Der aktuelle Wert von meineZahl ist: " + meineZahl);
+
+            // Zeichenkette einlesen:
+            Console.WriteLine("Bitte geben Sie einen Text ein:");
+            string eingabe = Console.ReadLine();
+            Console.WriteLine("Deine Eingabe war:" + eingabe);
+
+            // Zahl einlesen:
+            Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+            int neueZahl = int.Parse(Console.ReadLine());
+            Console.WriteLine(neueZahl);
+
+
+            // Berechnung (falsch !!!!)
+
+            Console.WriteLine("Bitte geben Sie zwei Zahlen ein:");
+            string zahl1 = Console.ReadLine();
+            string zahl2 = Console.ReadLine();
+
+            // -> Berechnung: + Operator
+
+            string ergebnis = zahl1 + zahl2;
+
+            Console.WriteLine(ergebnis);
+            // Übung: Datentypen korrigieren damit die Berechnung funktioniert 
+
+
             Console.ReadKey();
             // Programm starten: F5
         }
