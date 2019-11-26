2b78eedfb18ab821d40d6fb4c163025cf0233d0f
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-26 12:19:17 +0100

    Init OOP

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP // Objektorientierte Programmieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
