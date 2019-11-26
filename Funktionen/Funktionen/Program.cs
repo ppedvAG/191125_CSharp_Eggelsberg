284e2aac085e58db41122a67461714745089c189
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-26 09:32:48 +0100

    Init Funktionen

---
+++ Funktionen/Funktionen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funktionen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
