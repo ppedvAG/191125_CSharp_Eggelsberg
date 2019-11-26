a8cb61b5145658ed2864f542f420516c3f1b751b
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-26 10:12:56 +0100

    Optionale Parameter

--- Funktionen/Funktionen/Program.cs
+++ Funktionen/Funktionen/Program.cs
@@ -36,6 +36,18 @@ namespace Funktionen
                 Console.WriteLine("Es war etwas anderes.");
             }
 
+            // Überladen von Methoden
+            Console.WriteLine(Addieren(12,3));
+            Console.WriteLine(Addieren(12,3,5));
+
+            // params
+            Console.WriteLine(Sum(1,2,3));
+            Console.WriteLine(Sum(1));
+            Console.WriteLine(Sum(1,6,3,1,3,4,7,9,9,6,4,2,2,3,4,6));
+
+            MachEtwas("Hallo");
+            MachEtwas("Welt",99);
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
@@ -51,5 +63,29 @@ namespace Funktionen
         // Mini-Übung:
         // Funktion: Addieren: 2 Zahlen kommen rein, eine raus 
 
+
+        static int Addieren(int z1, int z2)
+        {
+            return z1 + z2;
+        }
+        static int Addieren(int z1, int z2,int z3)
+        {
+            return z1 + z2 + z3;
+        }
+        static int Addieren(int z1, int z2, int z3, int z4)
+        {
+            return z1 + z2 + z3 + z4;
+        }
+
+        static int Sum(params int[] zahlen)
+        {
+            return zahlen.Sum();
+        }
+
+
+        static void MachEtwas(string text, int zahl = 5)
+        {
+            Console.WriteLine("Mach Etwas mit dem Text: " + text +  " und der Zahl " + zahl);
+        }
     }
 }
