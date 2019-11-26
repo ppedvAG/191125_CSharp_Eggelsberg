9d4f68f969b1c459fe62a1984aaa143bd8cdcd5f
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-26 15:27:23 +0100

    Übung Konto

---
+++ MeineBank/MeineBank/Konto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeineBank
+{
+    class Konto
+    {
+        // Konstruktor
+        public Konto(string inhaber, decimal kontostand)
+        {
+            Inhaber = inhaber;
+            Kontostand = kontostand;
+        }
+
+        // Autoproperty
+        public string Inhaber { get; set; } // Autoproperty, da keine Validierung notwending ist
+
+        // Feld
+        private decimal kontostand;
+
+        // Property
+        public decimal Kontostand
+        {
+            get { return kontostand; }
+            private set 
+            {
+                if(value > 0)
+                    kontostand = value; 
+                else
+                    Console.WriteLine("Kontostand darf nicht kleiner als 0 werden !!!");
+            }
+        }
+
+        // Methode
+        public void Einzahlen(decimal betrag)
+        {
+            if(betrag > 0)
+                Kontostand += betrag;
+                // Kontostand = Kontostand + betrag;
+            else
+                Console.WriteLine("Sie dürfen keinen negativen Betrag einzahlen !!!");
+        }
+
+        // Methode
+        public void Abheben (decimal betrag)
+        {
+            if (betrag > 0)
+                Kontostand -= betrag;
+            else
+                Console.WriteLine("Sie dürfen keinen negativen Betrag abheben");
+        }
+
+    }
+}
--- MeineBank/MeineBank/Program.cs
+++ MeineBank/MeineBank/Program.cs
@@ -9,6 +9,17 @@ namespace MeineBank
     {
         static void Main(string[] args)
         {
+            Konto k1 = new Konto("Michael Zöhling", 10000000);
+
+            Console.WriteLine(k1.Kontostand);
+            k1.Abheben(5000000);
+            Console.WriteLine(k1.Kontostand);
+
+            k1.Einzahlen(2);
+            Console.WriteLine(k1.Kontostand);
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
