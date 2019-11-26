c25cbca869f031710c1e296aa7ba637635d00a7d
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-26 10:04:08 +0100

    Funktion mit Parameter

--- Funktionen/Funktionen/Program.cs
+++ Funktionen/Funktionen/Program.cs
@@ -9,9 +9,47 @@ namespace Funktionen
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+            int zahl1 = int.Parse(Console.ReadLine());
+
+            // "Hochkomplizierte Logik"
+            int erg1 = Program.Verdoppeln(zahl1);   // Variante 1
+            int erg2 = Verdoppeln(44);              // Variante 2
+
+            Console.WriteLine(erg1);
+            Console.WriteLine(erg2);
+
+
+            // Funktion mit 2 Ausgaben:
+
+            Console.WriteLine("Bitte geben Sie eine ganze Zahl ein:");
+            string eingabe = Console.ReadLine();
+
+            int konvertierteZahl;
+
+            if(int.TryParse(eingabe,out konvertierteZahl) == true)
+            {
+                Console.WriteLine("Es war eine Zahl: " + konvertierteZahl);
+            }
+            else
+            {
+                Console.WriteLine("Es war etwas anderes.");
+            }
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
+
+
+        // Eigene Funktion:
+        static int Verdoppeln(int x)
+        {
+            int ergebnis = x * 2;
+            return ergebnis;
+        }
+
+        // Mini-Übung:
+        // Funktion: Addieren: 2 Zahlen kommen rein, eine raus 
+
     }
 }
