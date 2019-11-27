45958c5de2c0ac508e1e6491001b3354a9a8d186
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-27 13:29:58 +0100

    TryCatch im TryCatch ;)

--- Fehlerbehandlung/Fehlerbehandlung/Program.cs
+++ Fehlerbehandlung/Fehlerbehandlung/Program.cs
@@ -9,31 +9,48 @@ namespace Fehlerbehandlung
     {
         static void Main(string[] args)
         {
-
-            // try + TAB + TAB
             try
             {
-                Console.WriteLine("Bitte geben Sie eine Zahl ein:");
-                int zahl = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Deine Eingabe war: " + zahl);
-            }
-            catch(FormatException)
-            {
-                Console.WriteLine("Bitte geben Sie nur ganze Zahlen ein !");
-            }
-            catch(OverflowException)
-            {
-                Console.WriteLine("Deine Zahl war zu groß oder zu klein");
-            }
-            //catch (Exception ex)
-            //{
-            //    //Console.WriteLine("FEHLER !!!!!!11111ELF!!!");
-            //    Console.WriteLine(ex.Message);
-            //    // throw; // <--- Fehler ausgeben
-            //}
 
 
-            
+
+                // try + TAB + TAB
+                try
+                {
+                    Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+                    int zahl = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Deine Eingabe war: " + zahl);
+
+                    // Fehler selber auslösen
+                    // throw new InvalidOperationException("FEHLER HAHAHHAHA");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bitte geben Sie nur ganze Zahlen ein !");
+                    throw new DivideByZeroException();
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Deine Zahl war zu groß oder zu klein");
+                }
+                finally
+                {
+                    Console.WriteLine("Codeblock, der IMMER ausgeführt wird");
+                }
+
+                //catch (Exception ex)
+                //{
+                //    //Console.WriteLine("FEHLER !!!!!!11111ELF!!!");
+                //    Console.WriteLine(ex.Message);
+                //    // throw; // <--- Fehler ausgeben
+                //}
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("wir kommen aus der DivideByZero-Exception ;) ");
+            }
+
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
