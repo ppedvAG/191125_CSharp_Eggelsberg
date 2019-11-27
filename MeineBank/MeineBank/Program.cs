86fae4eb7d9a09c509b0cef10e11b8c500245439
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-27 17:01:36 +0100

    Pro-Version der Übung mit unterschiedlichen Listentypen

--- MeineBank/MeineBank/Program.cs
+++ MeineBank/MeineBank/Program.cs
@@ -75,6 +75,48 @@ namespace MeineBank
 
             }
 
+            // Variante Stack/Queue
+
+            Stack<JugendKonto> jugendStack = new Stack<JugendKonto>();
+            jugendStack.Push(new JugendKonto("Tom Ate", 1000));
+            jugendStack.Push(new JugendKonto("Anna Nass", 1000));
+            jugendStack.Push(new JugendKonto("Peter Silie", 1000));
+            jugendStack.Push(new JugendKonto("Franz Ose", 1000));
+            jugendStack.Push(new JugendKonto("Martha Pfahl", 1000));
+
+            Queue<GiroKonto> giroQueue = new Queue<GiroKonto>();
+            giroQueue.Enqueue(new GiroKonto("Bill Dung", 1000));
+            giroQueue.Enqueue(new GiroKonto("Axel Schweiß", 1000));
+            giroQueue.Enqueue(new GiroKonto("Anna Bolika", 1000));
+            giroQueue.Enqueue(new GiroKonto("Albert Tross", 1000));
+            giroQueue.Enqueue(new GiroKonto("Frank N Stein", 1000));
+
+            // 2) Mit einer Schleife die Überweisung starten
+            //Random generator = new Random();
+
+
+            for (int i = 0; i < 5; i++) // wichtig ! Count statt Length,da es kein Array ist
+            {
+                try
+                {
+                    JugendKonto j = jugendStack.Pop(); // Oben aus dem Stack herausnehmen
+                    GiroKonto g = giroQueue.Dequeue(); // Unten aus der Liste nehmen
+
+                    j.Überweisung(generator.Next(0, 200),g);
+                    Console.WriteLine(" Stack/Queue - Transaktion Nr " + (i + 1)); // keinesfalls i++ nehmen !!! 
+
+                    Console.WriteLine($"JK {j.Inhaber}: {j.Kontostand}");
+                    Console.WriteLine($"GK {g.Inhaber}: {g.Kontostand}");
+                    Console.WriteLine("----------------");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Überweisung abgebrochen: Betrag ist zu hoch! ");
+                }
+
+            }
+
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
