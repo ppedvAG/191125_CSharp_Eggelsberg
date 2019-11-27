c504e3a8a9b6ecec3746bdfef20d4b6b9e5295a7
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-27 14:46:25 +0100

    List<T>

--- KomplexeDatentypen/KomplexeDatentypen/Program.cs
+++ KomplexeDatentypen/KomplexeDatentypen/Program.cs
@@ -9,9 +9,33 @@ namespace KomplexeDatentypen
     {
         static void Main(string[] args)
         {
+            // Arrays:
+            int[] zahlen = new int[5];
+            zahlen[0] = 12;
+            zahlen[1] = 9;
+            // ....
 
+            // List<T> (T ist ein Platzhalter)
 
+            List<int> zahlenListe = new List<int>();
+            List<string> stringListe = new List<string>();
+            // ...
 
+            zahlenListe.Add(12);
+            zahlenListe.Add(9);
+            zahlenListe.Add(7);
+            zahlenListe.Add(3);
+
+            Console.WriteLine(zahlenListe[0]);
+            Console.WriteLine(zahlenListe[1]);
+            Console.WriteLine(zahlenListe[2]);
+
+            // Praktische Features:
+
+            // zahlenListe.Remove(7);
+            // zahlenListe.Count; // anstelle von Length !!!
+            // zahlenListe.Clear(); // Alles löschen
+            // zahlenListe.Sort(); // Sortieren der Liste
 
             Console.WriteLine("---ANFANG---");
             Console.ReadKey();
