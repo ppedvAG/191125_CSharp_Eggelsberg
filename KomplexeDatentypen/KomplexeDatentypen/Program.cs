da45294001a12ec0c9137d7aa3d6648e73a9ca01
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-27 14:40:16 +0100

    Init KomplexeDatentypen

---
+++ KomplexeDatentypen/KomplexeDatentypen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KomplexeDatentypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("---ANFANG---");
+            Console.ReadKey();
+        }
+    }
+}
