b7cab20b0802f35dffa5b1c68729e2b42e576b9a
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-27 13:17:52 +0100

    Try/Catch

--- Fehlerbehandlung/Fehlerbehandlung/Program.cs
+++ Fehlerbehandlung/Fehlerbehandlung/Program.cs
@@ -10,6 +10,31 @@ namespace Fehlerbehandlung
         static void Main(string[] args)
         {
 
+            // try + TAB + TAB
+            try
+            {
+                Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+                int zahl = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Deine Eingabe war: " + zahl);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Bitte geben Sie nur ganze Zahlen ein !");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Deine Zahl war zu groß oder zu klein");
+            }
+            //catch (Exception ex)
+            //{
+            //    //Console.WriteLine("FEHLER !!!!!!11111ELF!!!");
+            //    Console.WriteLine(ex.Message);
+            //    // throw; // <--- Fehler ausgeben
+            //}
+
+
+            
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
