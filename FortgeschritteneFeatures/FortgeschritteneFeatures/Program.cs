367a2dc722473029eac46666058e8c9ecc4b7844
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-28 13:16:23 +0100

    Init Fortgeschrittene Features

---
+++ FortgeschritteneFeatures/FortgeschritteneFeatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FortgeschritteneFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("---ANFANG---");
+            Console.ReadKey();
+        }
+    }
+}
