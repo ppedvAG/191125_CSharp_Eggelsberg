d0b19dbbf9631c13548a4405e68323dbc95c961a
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-28 09:10:01 +0100

    Vererbungshierarchie

---
+++ Wiederholung_Tag3/Wiederholung_Tag3/Fahrzeug.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wiederholung_Tag3
+{
+    abstract class Fahrzeug
+    {
+        public int Geschwindigkeit { get; set; }
+
+        public abstract void Beschleunigen();
+        public abstract void Bremsen();
+    }
+
+    abstract class PrivatFahrzeug : Fahrzeug
+    {
+        public string Inhaber { get; set; }
+        public int Kilometerstand { get; set; }
+
+        public void Fahren(int stunden)
+        {
+            Kilometerstand += Geschwindigkeit * stunden;
+        }
+    }
+
+    class Auto : PrivatFahrzeug
+    {
+        public Auto(string Inhaber, int Kilometerstand)
+        {
+            this.Inhaber = Inhaber;
+            this.Kilometerstand = Kilometerstand;
+        }
+
+
+        public override void Beschleunigen()
+        {
+            Geschwindigkeit += 20;
+        }
+
+        public override void Bremsen()
+        {
+            Geschwindigkeit -= 20;
+        }
+    }
+
+    class Motorrad : PrivatFahrzeug
+    {
+        public override void Beschleunigen()
+        {
+            Geschwindigkeit += 30;
+        }
+
+        public override void Bremsen()
+        {
+            Geschwindigkeit -= 30;
+        }
+    }
+
+
+    interface ITransportfahrzeug 
+    {
+        int AnzahlPersonen { get; set; }
+        int MaximalesLadegewicht { get; set; }
+        bool GefahrengutTransport { get; set; }
+    }
+
+    class Schiff : Fahrzeug, ITransportfahrzeug
+    {
+        public int AnzahlPersonen { get; set; }
+        public int MaximalesLadegewicht { get; set; }
+        public bool GefahrengutTransport { get; set; }
+
+        public override void Beschleunigen()
+        {
+            Geschwindigkeit += 10;
+        }
+
+        public override void Bremsen()
+        {
+            Geschwindigkeit -= 10;
+        }
+    }
+
+
+    class Flugzeug : Fahrzeug, ITransportfahrzeug
+    {
+        public int AnzahlPersonen { get; set; }
+        public int MaximalesLadegewicht { get; set; }
+        public bool GefahrengutTransport { get; set; }
+        public int Tragfläche { get; set; }
+
+        public override void Beschleunigen()
+        {
+            Geschwindigkeit += 80;
+        }
+
+        public override void Bremsen()
+        {
+            Geschwindigkeit -= 80;
+        }
+    }
+
+
+    class LKW : PrivatFahrzeug, ITransportfahrzeug
+    {
+        public int AnzahlPersonen { get; set; }
+        public int MaximalesLadegewicht { get; set; }
+        public bool GefahrengutTransport { get; set; }
+        public double Höhe { get; set; }
+
+        public override void Beschleunigen()
+        {
+            Geschwindigkeit += 30;
+        }
+
+        public override void Bremsen()
+        {
+            Geschwindigkeit -= 30;
+        }
+    }
+}
--- Wiederholung_Tag3/Wiederholung_Tag3/Program.cs
+++ Wiederholung_Tag3/Wiederholung_Tag3/Program.cs
@@ -9,6 +9,31 @@ namespace Wiederholung_Tag3
     {
         static void Main(string[] args)
         {
+            Auto audi = new Auto("Michi Z",1234);
+
+            Console.WriteLine(audi.Kilometerstand);
+            audi.Beschleunigen();
+            audi.Beschleunigen();
+            audi.Beschleunigen();
+
+            audi.Fahren(3);
+
+            Console.WriteLine(audi.Kilometerstand);
+
+
+            LKW man = new LKW();
+            man.GefahrengutTransport = false;
+            man.Höhe = 2.5;
+            man.MaximalesLadegewicht = 2000;
+
+            man.Beschleunigen();
+            man.Beschleunigen();
+            man.Beschleunigen();
+
+            man.Fahren(10);
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
