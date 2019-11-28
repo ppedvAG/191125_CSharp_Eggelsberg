16747a8ad0a7e8d6326902decc4b8126de8f297a
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-28 09:19:15 +0100

    Vergleich

--- Wiederholung_Tag3/Wiederholung_Tag3/Program.cs
+++ Wiederholung_Tag3/Wiederholung_Tag3/Program.cs
@@ -20,7 +20,6 @@ namespace Wiederholung_Tag3
 
             Console.WriteLine(audi.Kilometerstand);
 
-
             LKW man = new LKW();
             man.GefahrengutTransport = false;
             man.Höhe = 2.5;
@@ -31,6 +30,34 @@ namespace Wiederholung_Tag3
             man.Beschleunigen();
 
             man.Fahren(10);
+
+            // Teil 2:
+
+
+            // Ziel: Liste erstellen, in der alle PrivatFahrzeuge gespeichert werden können
+
+            // PrivatFahrzeug[]
+            // List<PrivatFahrzeug>
+
+            // Liste für alle Fahrzeuge (Auto, Schiff, Flugzeug)
+
+            List<Fahrzeug> meineGarage = new List<Fahrzeug>();
+            meineGarage.Add(new Schiff());
+            meineGarage.Add(new Flugzeug());
+
+            List<ITransportfahrzeug> meinSpeditionsUnternehmen = new List<ITransportfahrzeug>();
+            meinSpeditionsUnternehmen.Add(new LKW());
+            meinSpeditionsUnternehmen.Add(new Schiff());
+
+            // meinSpeditionsUnternehmen[0];
+            // meineGarage[0].
+
+            // if(meineGarage[0].Equals(meinSpeditionsUnternehmen[0])) // Wertevergleich
+            if(meineGarage[0] is Schiff)
+            {
+                // mach xyz mit dem Schiff
+            }
+
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
